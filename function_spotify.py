c95597ebb97df8fe2052d7d5e1b214097bb30410
Arnaud CHATELAIN <a.chatelain@actongroup.com>
2024-10-15 20:57:04 +0200

    ADD some function

--- function_spotify.py
+++ function_spotify.py
@@ -2,15 +2,17 @@ import requests
 import json
 import webbrowser
 import config
+from collections import Counter
 
 user_id_arnaud = '11122778696'
-CLIENT_SECRET = config.CLIENT_SECRET
+client_secret = config.CLIENT_SECRET
 redirect_uri = config.REDIRECT_URI
 REDIRECT_URI='http://localhost:8889/callback'
 BASE_URL = 'https://api.spotify.com/v1/'
+client_id = config.CLIENT_ID
 
 # GET code for scope 
-def get_code_with_scope(CLIENT_ID, *additional_scopes):
+def get_code_with_scope(CLIENT_ID = client_id, *additional_scopes):
     # Impression des valeurs pour débogage
     # Liste des scopes de base
     base_scopes = [
@@ -30,7 +32,7 @@ def get_code_with_scope(CLIENT_ID, *additional_scopes):
     webbrowser.open(auth_url)
 
 #Creer le header a partir du code
-def create_header(code): 
+def create_header(code, CLIENT_ID = client_id, CLIENT_SECRET = client_secret): 
     
     # Préparer les données pour l'échange de code
     data = {
@@ -53,7 +55,7 @@ def create_header(code):
     return headers
 
 # Recuperer la liste des Playlists d'un user
-def get_playlist_for_user(headers = headers, user = user_id_arnaud, limits = '50') :
+def get_playlist_for_user(headers, user = user_id_arnaud, limits = '50') :
     url = f"{BASE_URL}users/{user}/playlists?limit={limits}"
     response = requests.get(url, headers=headers)
     playlists = response.json()
@@ -62,32 +64,81 @@ def get_playlist_for_user(headers = headers, user = user_id_arnaud, limits = '50
     for playlist in playlists['items']:
        print(playlist['name'], ' --- ', playlist['href'])
 
+
+# fonction qui print la liste des tracks
+def print_track_for_playlist(headers, PLAYLIST) :
+    url = f"{BASE_URL}playlists/{PLAYLIST}"
+    response = requests.get(url, headers=headers)
+    song_playlist = response.json()
+    for song in song_playlist['tracks']['items']:
+        print(song['track']['name'], ' --- ', song['track']['artists'][0]['name'], '---', song['track']['uri'])
+        uris_dict["uris"].append(song['track']['uri'])
+
 # fonction qui retourne une liste d'uri des tracks
-def get_track_for_playlist(playlist, headers = headers) :
-    list_uris = []
-    for song in playlist['tracks']['items']:
-        list_uris.append(song['track']['uri'])
-    return list_uris
+def get_track_for_playlist(headers, PLAYLIST) :
+    uris_dict = {"uris": []}
+    url = f"{BASE_URL}playlists/{PLAYLIST}"
+    response = requests.get(url, headers=headers)
+    song_playlist = response.json()
+    for song in song_playlist['tracks']['items']:
+        uris_dict["uris"].append(song['track']['uri'])
+    return uris_dict
 
 #Créer une playliste
-def create_playlist(playlist_name, headers = headers, user = user_id_arnaud) :
+def create_playlist(headers, playlist_name, user = user_id_arnaud) :
     new_playlist = json.dumps({
       "name": playlist_name,
       "description": "create by API",
       "public": False
       })
     #création d'une playlist
-    requests.post(BASE_URL  + 'users/'+ user +'/playlists',  data=new_playlist, headers=headers)
+    response = requests.post(BASE_URL  + 'users/'+ user +'/playlists',  data=new_playlist, headers=headers)
     return response.json()
 
-def intersection_listes(*listes):
-    # Convertir les listes en ensembles pour utiliser l'intersection
-    ensembles = map(set, listes)
-    # Calculer l'intersection de tous les ensembles
-    resultat = set.intersection(*ensembles)
-    # Convertir le résultat en liste
-    return list(resultat)
+def merge_playlist(headers, output_playlist_id, input_playlist_id, user = user_id_arnaud):
+    uris_dict_to_merge = get_track_for_playlist(headers, input_playlist_id)
+    response = requests.post(BASE_URL + 'playlists/' + output_playlist_id +'/tracks', json=uris_dict_to_merge, headers=headers)
+    print(response.status_code)
+    return response.json()
 
+# Recuperer la liste des tracks en plusieurs exemplaire
+def find_duplicate_track(headers, playlist_id):
+    uris_dict_songs = get_track_for_playlist(headers, playlist_id)
+    uri_counts = Counter(uris_dict_songs['uris'])
+    # Extraire les URI qui apparaissent plus d'une fois
+    duplicates_list = [uri for uri, count in uri_counts.items() if count > 1]
+    # Construire la liste de dictionnaires pour les pistes
+    tracks_list = [{"uri": uri} for uri in duplicates_list]
+    print(tracks_list)
+    return tracks_list
+
+def delete_duplicate_track(headers, playlist_id):
+    uris_duplicate_songs = find_duplicate_track(headers, playlist_id)
+    # Recupere le snapshot_id
+    snapshot_id= requests.get(BASE_URL + 'playlists/' + playlist_id, headers=headers).json()['snapshot_id']
+    # Construire le dictionnaire final 
+    data = {
+        "tracks": uris_duplicate_songs,
+        "snapshot_id": snapshot_id
+    }
+    print(data)
+    #response = requests.delete(BASE_URL + 'playlists/' + playlist_id, data=data, headers=headers)
+    #print(response.status_code)
+    #return response.json()
+
+#def intersection_listes(*listes):
+#    # Convertir les listes en ensembles pour utiliser l'intersection
+#    ensembles = map(set, listes)
+#    # Calculer l'intersection de tous les ensembles
+#    resultat = set.intersection(*ensembles)
+#    # Convertir le résultat en liste
+#    return list(resultat)
+
+
+def json_in_file(PATH, JSON):
+    with open(PATH, 'w') as file:
+      # Ecrire le JSON dans le fichier
+      json.dump(JSON, file, indent=1)
 
 #def main():
 #    get_code_with_scope()
